fd1d79ed6fb5e463274976b69f1f1be83435be67
Divyanshu Dimri <divyanshudimri@Divyanshus-MacBook-Air.local>
2018-12-21 16:29:49 +0530

    two points solved

--- task.php
+++ task.php
@@ -1,24 +1,32 @@
 <?php 
 function dayofweek($d, $m, $y) 
 { 
-  $l = ($y-1)%6;
-  $days = ($y-1)*186 - ($y-1)%6 + (($m-1)/2)*16 + (($m-1)-($m-1)/2)*15 + $d;
-  $mod = $days % 6;
-  if($mod == 1)
-      return 'monday';
-  if($mod == 2)
-      return 'tuesday';
-  if($mod == 3)
-      return 'wednesday';
-  if($mod == 4)
-      return 'thursday';
-  if($mod == 5)
-      return 'friday';
-  if($mod == 0)
-      return 'saturday';
+  $days = ($y-1)*186 - floor(($y-1)/6) + floor(($m-1)/2)*16 + (($m-1)-floor(($m-1)/2))*15 + $d;
+  $mod = $days % 5;
+      return $mod;
 } 
-
-// Driver Code 
+function numberofweek($d, $m, $y, $week) 
+{
+  if($m%2 <> 0)
+    { return 3;
+    } 
+    else {
+      $days = ($y-1)*186 - floor(($y-1)/6) + floor(($m-1)/2)*16 + (($m-1)-floor(($m-1)/2))*15 + 1;
+      $mod = $days % 5;
+      if ($week[$mod] == $d) {
+        return 4;
+      }
+      else{
+        return 3;
+      }
+    }
+      
+} 
+$dayKnown = 3131*186 - (3131%6) + 1;
+$week = array("0" => "Monday", "1" => "Tuesday", "2" => "Thursday", "3" => "Friday", "4" => "Saturday");
 $day = dayofweek(1,1,3132); 
-echo $day; 
+echo $week[$day]; 
+
+$day = numberofweek("Tuesday",1,3132,$week); 
+echo $day;
 ?>
